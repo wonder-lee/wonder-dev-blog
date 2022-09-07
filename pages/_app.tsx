8c1f47a372a27ddcfb3ca2f10897576ed5b1a642
wonder-lee <hi.wonder.dev@gmail.com>
2022-09-07 19:58:50 +0900

    sentry 적용 post 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -41,6 +41,7 @@ function MyApp({ Component, pageProps }: AppProps) {
               return (
                 <span
                   key={tag.query}
+                  onClick={() => onClickTagButton(tag.query)}
                   className={`${
                     (tag.query &&
                       router.asPath
@@ -51,7 +52,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                       : "text-xs mr-3 mb-4 px-3 py-2 rounded-xl hover:cursor-pointer bg-slate-50 shadow-[rgba(50, 50, 93, 0.25) 0px 30px 60px -12px,rgba(0, 0, 0, 0.3) 0px 18px 36px -18px]"
                   }`}
                 >
-                  <span onClick={() => onClickTagButton(tag.query)}>
+                  <span>
                     {tag.name}
                   </span>
                 </span>
