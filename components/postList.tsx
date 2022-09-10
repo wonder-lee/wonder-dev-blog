59c6b262b46d5677973acd7a8522db9b0dd038c8
wonder-lee <hi.wonder.dev@gmail.com>
2022-09-10 21:14:28 +0900

    220910 til 추가 및 post list tag column 위치 변경

--- components/postList.tsx
+++ components/postList.tsx
@@ -5,8 +5,8 @@ function PostList({ title, date, tag, slug, index, length }: any) {
     <Link href="/[slug]" as={`/${slug}`}>
       <tr className="text-sm hover:cursor-pointer hover:bg-slate-50">
         <td>{length - index}</td>
-        <td className="max-w-xs">{title}</td>
         <td className="min-w-50">{tag}</td>
+        <td className="max-w-xs">{title}</td>
         <td>{date}</td>
       </tr>
     </Link>
